81978d8daa96038f461b6bcb4c299d30c57b99ae
unknown <19252290@bsi.ac.id>
2026-06-24 09:10:04 +0700

    Menambahkan Isi Profile

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost"; 
+$host = "localhost";
 $user = "root";
 $pass = "";
-$db   = "web_berita1";
+$db = "programming_berita2_putra";
 
 $koneksi = mysqli_connect($host, $user, $pass, $db);
 if (!$koneksi) {
